a9f9fd7ac073acba4e66e3b1d0d9448c8a295f64
Nic Doorlay <n.doorlay@gmail.com>
2019-01-09 11:38:31 -0800

    Update script_two.py

--- script_two.py
+++ script_two.py
@@ -1,1 +1,9 @@
 # store terms in a data base via a text file
+# quizlet type of document
+
+def welcome():
+	print("Welcome to the vocabulary practice!)
+	name = input("What's your name?")
+	print("Welcome, {}! Let's begin." .format(name))
+		  
+  
