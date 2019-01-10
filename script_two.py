6b6dd152f6a93f38990c03dd0612d1fdb3afc9bf
Nic Doorlay <n.doorlay@gmail.com>
2019-01-10 11:05:57 -0800

    Update script_two.py

--- script_two.py
+++ script_two.py
@@ -1,10 +1,15 @@
 # store terms in a data base via a text file
 # quizlet type of document
 
+
+# Welcome message to be displayed upon initial running of the program
 def welcome():
 	print("Welcome to the vocabulary practice!")
 	name = input("What's your name?")
 	print("Welcome, {}! Let's begin." .format(name))
 		  
 
-welcome()
+# Name the database text file with the vocab, "vocab.txt"
+vocab_list = open("vocab.txt")
+vocab = vocab_list.splitlines()
+
