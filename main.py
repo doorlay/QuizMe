040f23e81c7c73d7eabe7f70c2b5ae71a68e4b0a
Nic Doorlay <n.doorlay@gmail.com>
2019-03-28 12:04:16 -0700

    Potentially fixed issue w/ typos

--- main.py
+++ main.py
@@ -28,52 +28,54 @@ definitions = definitions2.splitlines()
 
 # Function to ensure definitions and vocab are the same length
 def length_check():
-  a = len(definitions)
-  b = len(vocab)
-  if (a != b):
-    print("The definitions and vocab lists appear to not be the same length.")
-    print("Please update the lists before you continue.")
-  else:
-    return True
+	a = len(definitions)
+  	b = len(vocab)
+  	if (a != b):
+    		print("The definitions and vocab lists appear to not be the same length.")
+    		print("Please update the lists before you continue.")
+  	else:
+    		return True
 
 # Function that generates a random number between one and four, inclusive
 def random_one_to_four():
-  randomnum = random.randint(1,4)
-  return randomnum
+  	randomnum = random.randint(1,4)
+  	return randomnum
 
 # Function that generates a random number between entire index of vocab list
 def random_length():
-  a = len(vocab) - 1
-  randomnum = random.randint(0,a)
-  return randomnum
+  	a = len(vocab) - 1
+  	randomnum = random.randint(0,a)
+  	return randomnum
 
 
 # Function that asks the multiple choice questions.
 def frquestion(n):
-  # Initial question for the user
-  print("Would you like to answer with terms or definitions?")
+  	# Initial question for the user
+  	print("Would you like to answer with terms or definitions?")
 
-  # Ask this so we can alternate between asking displaying vocab and definitions.
-  choice = input("Type 'terms' or 'definitions':")
+  	# Creates a flag to use in a while loop
+  	flag = False;
+  	while flag is False:
+		choice = input("Type 'terms' or 'definitions':")
+  		if choice == "definitions":
+    			choice_two = "What is the definition of"
+    			choice_three = vocab2.splitlines()
+    			# We assign 0 to a, so we know that the user is answering with definitions
+    			a = 0
+			flag = True;
+  		elif choice == "terms":
+    			choice_two = "What is the term for"
+    			choice_three = definitions2.splitlines()
+    			# We assign 1 to a, so we know that the user is answering with terms
+    			a = 1
+			flag = True;
+  		else:
+    			choice_two = "You didn't type either."
+  		
+		print("{} {}?" .format(choice_two, choice_three[n] ))
 
-
-  if choice == "definitions":
-    choice_two = "What is the definition of"
-    choice_three = vocab2.splitlines()
-    # We assign 0 to a, so we know that the user is answering with definitions
-    a = 0
-  elif choice == "terms":
-    choice_two = "What is the term for"
-    choice_three = definitions2.splitlines()
-    # We assign 1 to a, so we know that the user is answering with terms
-    a = 1
-  else:
-    choice_two = "You didn't type either."
-    choice_three = "Please restart."
-  print("{} {}?" .format(choice_two, choice_three[n] ))
-
-  # Return either 0 or 1 so we know if the user is answering terms or definitions
-  return a
+  		# Return either 0 or 1 so we know if the user is answering terms or definitions
+  		return a
 
 
 # Sub-function for checking multiple choice answers
@@ -81,61 +83,61 @@ def frquestion(n):
 # 'answer' is the users answer
 # a is the index of the correct answer
 def frcheck(b, answer, a):
-  if b == 0:
+  	if b == 0:
 
-    # .lower() method is used to make sure case sensitivity while answering is not an issue
-    if answer.lower() == definitions[a].lower():
-      print("Correct!")
-      # Returns 1 if the answer is correct
-      return 1
-    else:
-      print("Wrong!")
-      # This prints out the correct answer.
-      print("The correct answer was {}." .format(definitions[a]))
-      # Returns 0 if the answer is wrong
-      return 0
-  if b == 1:
-    if answer.lower() == vocab[a].lower():
-      print("Correct!")
-      # Returns 1 if the answer is correct
-      return 1
-    else:
-      print("Wrong!")
-      # This prints out the correct answer.
-      print("The correct answer was {}." .format(vocab[a]))
-      # Returns 0 if the answer is wrong
-      return 0
+    		# .lower() method is used to make sure case sensitivity while answering is not an issue
+    		if answer.lower() == definitions[a].lower():
+      			print("Correct!")
+      			# Returns 1 if the answer is correct
+      			return 1
+    		else:
+      			print("Wrong!")
+      			# This prints out the correct answer.
+      			print("The correct answer was {}." .format(definitions[a]))
+      			# Returns 0 if the answer is wrong
+      			return 0
+  	if b == 1:
+    		if answer.lower() == vocab[a].lower():
+      			print("Correct!")
+      			# Returns 1 if the answer is correct
+      			return 1
+    		else:
+      			print("Wrong!")
+      			# This prints out the correct answer.
+      			print("The correct answer was {}." .format(vocab[a]))
+      			# Returns 0 if the answer is wrong
+      			return 0
 
 # Function that evaluates the answers for multiple choice questions
 def fr():
-  # a is assigned to a random number in within the index of the list of vocab/definitions
-  a = random_length()
+  	# a is assigned to a random number in within the index of the list of vocab/definitions
+  	a = random_length()
 
-  # b is assigned to either 0 or 1, depending on if the user is inputting terms or definitions
-  b = frquestion(a)
+  	# b is assigned to either 0 or 1, depending on if the user is inputting terms or definitions
+  	b = frquestion(a)
 
-  # answer is assigned to the users answers
-  answer = input("Answer here:")
-  # c is assigned to 0 if the answer is wrong
-  # c is assigned to 1 if the answer is right
-  c = frcheck(b, answer, a)
-  return c
+  	# answer is assigned to the users answers
+  	answer = input("Answer here:")
+  	# c is assigned to 0 if the answer is wrong
+  	# c is assigned to 1 if the answer is right
+  	c = frcheck(b, answer, a)
+  	return c
 
 
 def quizstart():
-  welcome()
-  length_check()
+  	welcome()
+  	length_check()
   
 def quiz():
-  # This function sends the welcome message and ensures that the length of vocab and definitions are equal
-  quizstart()
+  	# This function sends the welcome message and ensures that the length of vocab and definitions are equal
+  	quizstart()
 
-  # c is assigned to the length of the vocab list, multiplied by two. This value is to be passed into the for loop, to determine the amount of questions to be asked.
-  c = len(vocab) * 2
+  	# c is assigned to the length of the vocab list, multiplied by two. This value is to be passed into the for loop, to determine the amount of questions to be asked.
+  	c = len(vocab) * 2
 
-  # b is assigned to either 0 or 1, depending on if answering is wrong or right, respectively
-  for x in range(1,c):
-    fr()
+  	# b is assigned to either 0 or 1, depending on if answering is wrong or right, respectively
+  	for x in range(1,c):
+    		fr()
 
 
 quiz()
